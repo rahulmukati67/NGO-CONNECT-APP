ba23556a31fecedd64cad5347c5c4b840ffc197a
Shrushti Nagar <Shrushtinagar220@gmail.com>
2023-10-10 02:53:13 +0530

    NGO Connect App : Fragments set up , Toolbar done , navigation items styling done

--- app/src/main/java/com/shrushti/ngoconnectapp/MainActivity.kt
+++ app/src/main/java/com/shrushti/ngoconnectapp/MainActivity.kt
@@ -6,12 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.navigation.NavigationView
 import com.shrushti.ngoconnectapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListener {
     private lateinit var binding: ActivityMainBinding
     private lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,26 +24,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        toggle = ActionBarDrawerToggle(this, binding.DrawerLayout, R.string.open, R.string.close)
-        binding.DrawerLayout.addDrawerListener(toggle)
+        setSupportActionBar(binding.toolbar)
+        binding.navView.setNavigationItemSelectedListener(this)
+
+        toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open, R.string.close)
+        binding.drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setHomeAsUpIndicator(R.drawable.baseline_menu_24)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.baseline_menu_open_24)
 
-        binding.rvNGOs.layoutManager = LinearLayoutManager(this)
-
-//        binding.NavigationView.setNavigationItemSelectedListener {
-//            when (it.itemId) {
-//                R.id.nav_profile -> {
-//                    val intent = Intent(this@MainActivity, ProfileActivity::class.java)
-//                    startActivity(intent)
-//                }
-//
-//            }
-//            true
-//        }
-
+        if(savedInstanceState == null){
+            replaceFragment(HomeFragment(),"")
+        }
 
     }
 
@@ -47,5 +44,27 @@ class MainActivity : AppCompatActivity() {
         if(toggle.onOptionsItemSelected(item)) return true
         return super.onOptionsItemSelected(item)
     }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_profile -> replaceFragment(ProfileFragment(),item.title.toString())
+            R.id.nav_volunteer ->replaceFragment(VolunteerFragment(),item.title.toString())
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if(binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        }else{
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment, title:String){
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container,fragment).commit()
+        supportActionBar?.title = title
+    }
+
 
 }
