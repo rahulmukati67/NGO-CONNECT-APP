c56da54cd02effe17a7393fee099d6f2278e6576
Shrushti Nagar <Shrushtinagar220@gmail.com>
2023-10-22 13:02:08 +0530

    Commit

--- app/src/main/java/com/project/ngoconnectapp/HomeFragment.kt
+++ app/src/main/java/com/project/ngoconnectapp/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -63,7 +64,7 @@ class HomeFragment : Fragment() {
             }
 
             override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
+                Toast.makeText(context,"${error.message}",Toast.LENGTH_SHORT).show()
             }
 
         })
--- app/src/main/java/com/project/ngoconnectapp/MainActivity.kt
+++ app/src/main/java/com/project/ngoconnectapp/MainActivity.kt
@@ -13,17 +13,18 @@ import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.project.ngoconnectapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var binding: ActivityMainBinding
     private lateinit var toggle: ActionBarDrawerToggle
     private lateinit var auth: FirebaseAuth
+    private lateinit var  tvUserName : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
-        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer,HomeFragment()).commit()
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, HomeFragment()).commit()
         setSupportActionBar(binding.toolbar)
 
         auth = FirebaseAuth.getInstance()
@@ -40,57 +41,83 @@ class MainActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelect
 
         val headerLayout = binding.navView.getHeaderView(0)
         val imgBack = headerLayout.findViewById<ImageButton>(R.id.imgBack)
-        val tvUserName = headerLayout.findViewById<TextView>(R.id.tvName)
-        if(auth.currentUser != null){
-            tvUserName.text = ""
-        }
-        else{
+        tvUserName = headerLayout.findViewById(R.id.tvName)
+
+        if (auth.currentUser != null) {
+            tvUserName.text = auth.currentUser?.displayName ?: ""
+        } else {
             tvUserName.text = "Login/Register"
         }
+
         imgBack.setOnClickListener {
             binding.drawerLayout.closeDrawer(GravityCompat.START)
         }
-        
+
+        tvUserName.setOnClickListener {
+            if(tvUserName.text == "Login/Register"){
+                startActivity(Intent(this,RegistrationActivity::class.java))
+            }
+        }
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(toggle.onOptionsItemSelected(item)) return true
+        if (toggle.onOptionsItemSelected(item)) return true
         return super.onOptionsItemSelected(item)
     }
+
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
+        when (item.itemId) {
             R.id.nav_profile -> {
-                if(auth.currentUser != null){
-                    val intent = Intent(this,ProfileActivity::class.java)
+                if (auth.currentUser != null) {
+                    startActivity(Intent(this, ProfileActivity::class.java))
+                } else {
+                    Toast.makeText(this, "REGISTER / LOGIN FIRST !", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+            R.id.nav_myDonations -> {
+                if (auth.currentUser != null) {
+                    val intent = Intent(this, MyDonationsActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    val intent = Intent(this, RegistrationActivity::class.java)
+                    Toast.makeText(
+                        this,
+                        "To donate , you have to Register/Login  !",
+                        Toast.LENGTH_LONG
+                    ).show()
                     startActivity(intent)
                 }
-                else{
-                    Toast.makeText(this,"LOGIN FIRST !",Toast.LENGTH_SHORT).show()
+
+            }
+
+            R.id.nav_volunteer -> {
+                if (auth.currentUser != null) {
+                    val intent = Intent(this, VolunteerActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    val intent = Intent(this, RegistrationActivity::class.java)
+                    Toast.makeText(
+                        this,
+                        "To  join as a volunteer , you have to Register/Login !",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    startActivity(intent)
                 }
 
             }
-            R.id.nav_myDonations ->{
-                if(auth.currentUser != null){
-                    val intent2 = Intent(this,MyDonationsActivity::class.java)
-                    startActivity(intent2)
-                }
-                else{
-                    val intent2 = Intent(this, RegistrationActivity::class.java)
-                    Toast.makeText(this,"To donate , you have to Register first !",Toast.LENGTH_LONG).show()
-                    startActivity(intent2)
-                }
 
-            }
-            R.id.nav_volunteer ->{
-                val intent3 = Intent(this, RegistrationActivity::class.java)
-                Toast.makeText(this,"To volunteer , you have to Register first !",Toast.LENGTH_LONG).show()
-                startActivity(intent3)
+            R.id.nav_logout->{
+                auth.signOut()
+                Toast.makeText(this,"Successfully Logged Out !",Toast.LENGTH_SHORT).show()
+                tvUserName.text ="Login/Register"
             }
         }
+
         binding.drawerLayout.closeDrawer(GravityCompat.START)
         return true
     }
-
-
 
 }
