8d309ebb9a55e15729b3d3287dd61867216a85b6
Shrushti Nagar <Shrushtinagar220@gmail.com>
2023-11-03 00:03:14 +0530

    Commit : minor changes

--- app/src/main/java/com/project/ngoconnectapp/ProfileActivity.kt
+++ app/src/main/java/com/project/ngoconnectapp/ProfileActivity.kt
@@ -4,12 +4,7 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.childEvents
 
 class ProfileActivity : AppCompatActivity() {
     private  lateinit var  profileEmail : TextView
