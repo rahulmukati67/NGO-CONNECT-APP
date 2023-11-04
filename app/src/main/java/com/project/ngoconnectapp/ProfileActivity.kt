cd31bd3586551bcb6d24d77713c5547bfce396f2
Shrushti Nagar <Shrushtinagar220@gmail.com>
2023-11-04 15:17:29 +0530

    Commit : Images uploaded from gallery to Firebase Storage and Also , fetched from firebase storage.

--- app/src/main/java/com/project/ngoconnectapp/ProfileActivity.kt
+++ app/src/main/java/com/project/ngoconnectapp/ProfileActivity.kt
@@ -2,7 +2,9 @@ package com.project.ngoconnectapp
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
@@ -10,12 +12,11 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
 import com.project.ngoconnectapp.databinding.ActivityProfileBinding
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
+import java.io.File
 
 class ProfileActivity : AppCompatActivity() {
     private lateinit var binding: ActivityProfileBinding
+    private val localFile = File.createTempFile("tempImage", "jpg")
     private lateinit var auth: FirebaseAuth
     private lateinit var dbRef: FirebaseDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +27,15 @@ class ProfileActivity : AppCompatActivity() {
 
 
         auth = FirebaseAuth.getInstance()
+        val ref =  FirebaseStorage.getInstance().reference.child("images/${auth.currentUser?.uid!!}.jpg")
+        ref.getFile(localFile).addOnSuccessListener {
+            val bitmap = BitmapFactory.decodeFile(localFile.absolutePath)
+            binding.imgProfile.setImageBitmap(bitmap)
+
+        }.addOnFailureListener{
+            Toast.makeText(this, "Failed to Upload File", Toast.LENGTH_SHORT).show()
+        }
+
 
         dbRef = FirebaseDatabase.getInstance()
         dbRef.getReference("users").child(auth.currentUser?.uid.toString()).get()
@@ -39,48 +49,64 @@ class ProfileActivity : AppCompatActivity() {
                 if (email != null) {
                     binding.profileEmail.text = email.toString()
                 } else {
-                    binding.profileEmail.text = "Not Found"
+                    binding.profileEmail.text = getString(R.string.nt_fd)
                 }
                 if (username != null) {
                     binding.profileName.text = username.toString()
                 } else {
-                    binding.profileName.text = "Not Found"
+                    binding.profileName.text = getString(R.string.nt_fd)
                 }
 
                 if (phoneNumber != null) {
                     binding.profileNumber.text = phoneNumber.toString()
                 } else {
-                    binding.profileNumber.text = "Not Found"
+                    binding.profileNumber.text = getString(R.string.nt_fd)
                 }
 
             }
 
-        val startForResult =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
-                if (result.resultCode == Activity.RESULT_OK) {
-                    val intent = result.data
-                    val uri = intent?.data
 
-                    val formatter = SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.CANADA)
-                    val filename = formatter.format(Date())
-                    val storageReference = FirebaseStorage.getInstance().getReference("images/" + "${auth.currentUser?.uid!!}/" + filename )
-                    storageReference.putFile(uri!!)
-
-                    binding.imgProfile.setImageURI(uri)
-                }
-            }
 
         binding.ivEdit.setOnClickListener {
-            val getIntent = Intent(Intent.ACTION_GET_CONTENT)
-            getIntent.type = "image/*"
-
-            val pickIntent = Intent( Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI )
-            pickIntent.type = "image/*"
-
-            val chooserIntent = Intent.createChooser(getIntent,"Select Image")
-            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(Intent(pickIntent)))
-            startForResult.launch(chooserIntent)
+            getImage()
         }
+    }
+
+    private val startForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val intent = result.data
+                val uri = intent?.data
+
+                val storageReference = FirebaseStorage.getInstance().getReference("images/" + "${auth.currentUser?.uid!!}.jpg" )
+                storageReference.putFile(uri!!).addOnSuccessListener {
+
+                    val stoRef =  FirebaseStorage.getInstance().reference.child("images/${auth.currentUser?.uid!!}.jpg")
+                    stoRef.getFile(localFile).addOnSuccessListener {
+                        val bitmap = BitmapFactory.decodeFile(localFile.absolutePath)
+                        binding.imgProfile.setImageBitmap(bitmap)
+
+                    }.addOnFailureListener{
+                        Toast.makeText(this, "Failed to Upload File", Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+
+
+
+            }
+        }
+    private fun getImage(){
+
+        val getIntent = Intent(Intent.ACTION_GET_CONTENT)
+        getIntent.type = "image/*"
+
+        val pickIntent = Intent( Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI )
+        pickIntent.type = "image/*"
+
+        val chooserIntent = Intent.createChooser(getIntent,"Select Image")
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, arrayOf(Intent(pickIntent)))
+        startForResult.launch(chooserIntent)
     }
 
 
