6441d64b829feecfb247d6c8631a13237c70ff06
Shrushti Nagar <Shrushtinagar220@gmail.com>
2023-11-04 20:28:52 +0530

    Commit

--- app/src/main/java/com/project/ngoconnectapp/MainActivity.kt
+++ app/src/main/java/com/project/ngoconnectapp/MainActivity.kt
@@ -11,6 +11,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -50,7 +51,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         val headerLayout = binding.navView.getHeaderView(0)
         val imgBack = headerLayout.findViewById<ImageButton>(R.id.imgBack)
-        profileImage = headerLayout.findViewById<ImageView>(R.id.imgProfile)
+        profileImage = headerLayout.findViewById(R.id.imgProfile)
         tvUserName = headerLayout.findViewById(R.id.tvName)
 
         if (auth.currentUser != null) {
@@ -147,6 +148,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 auth.signOut()
                 Toast.makeText(this,"Successfully Logged Out !",Toast.LENGTH_SHORT).show()
                 tvUserName.text =getString(R.string.click_login)
+                profileImage.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.download))
             }
         }
 
