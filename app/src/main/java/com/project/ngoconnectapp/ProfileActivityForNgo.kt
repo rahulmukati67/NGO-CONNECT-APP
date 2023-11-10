dac605d133526cd7f1570a86070388ea433af596
Shrushti Nagar <Shrushtinagar220@gmail.com>
2023-11-10 14:45:52 +0530

    Commit : Profile Activity for NGO.

--- app/src/main/java/com/project/ngoconnectapp/ProfileActivityForNgo.kt
+++ app/src/main/java/com/project/ngoconnectapp/ProfileActivityForNgo.kt
@@ -6,6 +6,7 @@ import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import com.google.firebase.auth.FirebaseAuth
@@ -30,14 +31,15 @@ class ProfileActivityForNgo : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
         dbRef = FirebaseDatabase.getInstance()
         storageRef = FirebaseStorage.getInstance()
+
+        getImageFromFirebase()
+
         val regNumber = intent.getStringExtra("regId")
         binding.tvRegNumNgo.text = regNumber
 
-        dbRef.getReference("ngoDetails").child(regNumber!!).get().addOnCompleteListener {
+        getDataFromFirebase()
 
-            binding.tvNgoName.text = it.result.child("name").value.toString()
-            binding.tvTypeNgo.text = it.result.child("ngoType").value.toString()
-        }
+
         binding.ivEditPhotoNgo.setOnClickListener {
 
             val getIntent = Intent(Intent.ACTION_GET_CONTENT)
@@ -53,6 +55,7 @@ class ProfileActivityForNgo : AppCompatActivity() {
         }
 
     }
+
      private val startForResult =
          registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result : ActivityResult ->
              if(result.resultCode == Activity.RESULT_OK){
@@ -60,16 +63,45 @@ class ProfileActivityForNgo : AppCompatActivity() {
                  val uri = intent?.data
 
                  val ref = storageRef.getReference("ngoImages/${auth.currentUser?.uid}.jpg")
-                 ref.putFile(uri!!).addOnSuccessListener { task->
-                     val location = task.storage
-                     location.getFile(localFile).addOnSuccessListener {
-                         val bitmap = BitmapFactory.decodeFile(localFile.absolutePath)
-                         binding.imgNgoProfile.setImageBitmap(bitmap)
 
-                         binding.progressBar3.visibility = View.INVISIBLE
-                     }
+                 ref.putFile(uri!!).addOnSuccessListener {
+
+                     getImageFromFirebase()
+                 }.addOnFailureListener{
+                     Toast.makeText(this, "Failed to upload the image !", Toast.LENGTH_SHORT).show()
                  }
              }
+         }
 
+    private fun getImageFromFirebase(){
+        val ref = storageRef.reference.child("ngoImages/${auth.currentUser?.uid}.jpg")
+        ref.getFile(localFile).addOnSuccessListener {
+            val bitmap = BitmapFactory.decodeFile(localFile.absolutePath)
+            binding.imgNgoProfile.setImageBitmap(bitmap)
+
+            binding.progressBar3.visibility = View.INVISIBLE
+
+        }.addOnFailureListener {
+            Toast.makeText(this , "Upload the Profile Picture !", Toast.LENGTH_SHORT).show()
+            binding.progressBar3.visibility = View.INVISIBLE
+        }
      }
+
+    private fun getDataFromFirebase(){
+        val regNumber = intent.getStringExtra("regId")
+        dbRef.getReference("ngoDetails").child(regNumber!!).get().addOnCompleteListener {
+
+            var ngoName = it.result.child("name").value.toString()
+            var ngoType = it.result.child("ngoType").value.toString()
+            var ngoEmail = it.result.child("emailId").value.toString()
+            var ngoNumber = it.result.child("phoneNo").value.toString()
+            var ngoWebsite = it.result.child("ngoWeb").value.toString()
+
+            binding.tvNgoName.text = ngoName
+            binding.tvTypeNgo.text = ngoType
+            binding.tvEmailNgo.text = ngoEmail
+            binding.tvNumberNgo.text = ngoNumber
+            binding.tvWebsiteNgo.text = ngoWebsite
+        }
+    }
 }
