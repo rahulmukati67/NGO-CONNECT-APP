c85df8472cb69db14dcd7c6f7c9fb5000a8f488a
rahulmukati67 <rahulmukati20067@acropolis.in>
2023-11-11 21:33:18 +0530

    Donation Acitivty layout

--- app/src/main/java/com/project/ngoconnectapp/MyDonationsActivity.kt
+++ app/src/main/java/com/project/ngoconnectapp/MyDonationsActivity.kt
@@ -1,11 +1,28 @@
 package com.project.ngoconnectapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.project.ngoconnectapp.databinding.ActivityMyDonationsBinding
 
 class MyDonationsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMyDonationsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_donations)
+        binding = ActivityMyDonationsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnDonate.setOnClickListener{
+            val builder = AlertDialog.Builder(this@MyDonationsActivity ,androidx.appcompat.R.style.Theme_AppCompat_DayNight_Dialog_Alert)
+            builder.setTitle("Thanks").setMessage("You Have Successful Donated Amount : Rs " + binding.donationAmount.text.toString())
+            builder.setPositiveButton("OK" ) { dialogInterface, _ ->
+                dialogInterface.dismiss()
+            }
+            val alertDialog = builder.create()
+            alertDialog.show()
+        }
     }
 }
